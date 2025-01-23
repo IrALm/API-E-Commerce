b39e3b0aacd5a402e5583d6e09c845e53d766cff
Moïse Aganze Lwaboshi <moiseaganze76@gmail.com>
2025-01-23 22:08:49 +0100

    Suppression du dossier api

--- api/src/main/java/com.backend.api.repository/CategorieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.api.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.backend.api.model.Categorie;
-
-@Repository
-public interface CategorieRepository extends CrudRepository<Categorie, Long>{
-}
